29cd7d4e7be64d09ab43168c9045625da2490901
Mimo <mimoriam420@gmail.com>
2023-03-30 21:06:27 +0500

    Commit #5 - Authentication portion of the application

--- src/iam/authentication/dto/sign-in.dto.ts
+++ src/iam/authentication/dto/sign-in.dto.ts
@@ -1,1 +1,18 @@
-export class SignInDto {}
+import {
+  IsEmail,
+  IsNumberString,
+  IsOptional,
+  MinLength,
+} from 'class-validator';
+
+export class SignInDto {
+  @IsEmail()
+  readonly email: string;
+
+  @MinLength(5)
+  readonly password: string;
+
+  @IsOptional()
+  @IsNumberString()
+  tfaCode?: string;
+}
--- src/iam/authentication/dto/sign-up.dto.ts
+++ src/iam/authentication/dto/sign-up.dto.ts
@@ -1,1 +1,16 @@
-export class SignUpDto {}
+import { IsEmail, IsString, MinLength } from 'class-validator';
+import { Role } from '@prisma/client';
+
+export class SignUpDto {
+  @IsString()
+  readonly username: string;
+
+  @IsEmail()
+  readonly email: string;
+
+  @IsString()
+  readonly role: Role;
+
+  @MinLength(5)
+  readonly password: string;
+}
